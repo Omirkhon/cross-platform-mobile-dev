f6a47fe5fa99575b026471328019beddf711cf1b
ayaullyms <sailauayaulym05@gmail.com>
2025-04-01 15:18:52 +0500

    flutter/about-page

---
+++ android/app/src/main/kotlin/com/example/cross_platform_mobile_dev/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.cross_platform_mobile_dev
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/screens/about_page.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+
+class AboutPage extends StatelessWidget {
+  const AboutPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              // Logo
+              Image.asset(
+                'assets/logo.png', 
+                width: 200,
+              ),
+              const SizedBox(height: 20),
+
+              const Text(
+                "JUST DO IT!",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 32,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black87,
+                ),
+              ),
+              const SizedBox(height: 10),
+
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 30),
+                child: Text(
+                  "A Simple, Yet Effective Way to Be More Productive.",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.black54,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 30),
+                child: Text(
+                  "Developed by Issayeva Vera, sailau Ayaulym, Turgimbayev Amirkhanin the scope of the course “Crossplatform Development” at Astana IT University.\n"
+                  "Mentor : Assistant Professor Abzal Kyzyrkanov",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 14,
+                    color: Colors.black54,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+
+              const SizedBox(height: 50),
+              
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.deepPurple,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(25),
+                  ),
+                  padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 15),
+                ),
+                onPressed: () {
+                  print("Let's Start button pressed");
+                },
+                child: const Text(
+                  "Let's Start  →",
+                  style: TextStyle(fontSize: 18, color: Colors.white),
+                ),
+              ),
+              const SizedBox(height: 20),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
